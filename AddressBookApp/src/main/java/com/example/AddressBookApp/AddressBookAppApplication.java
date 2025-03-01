dc222604c0970436825064d922542b985fe58bb6
Deepak20singh <137925992+Deepak20singh@users.noreply.github.com>
2025-03-02 03:20:25 +0530

    [Deepak] Add. Code of working in different different profiles UC8

--- AddressBookApp/src/main/java/com/example/AddressBookApp/AddressBookAppApplication.java
+++ AddressBookApp/src/main/java/com/example/AddressBookApp/AddressBookAppApplication.java
@@ -4,14 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class AddressBookAppApplication {
 	private static final Logger log = LoggerFactory.getLogger(AddressBookAppApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookAppApplication.class, args);
-		log.info("App has started!!!");
+		ApplicationContext context = SpringApplication.run(AddressBookAppApplication.class, args);
+		log.info("Application has started in {} environment",
+				context.getEnvironment().getProperty("environment"));
 	}
 
 }
