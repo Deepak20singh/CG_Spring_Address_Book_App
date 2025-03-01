d2d6647b4cb1e3e95bad3f68cfddb506a4c54cd0
Deepak20singh <137925992+Deepak20singh@users.noreply.github.com>
2025-03-02 03:08:59 +0530

    [Deepak] Add. Code of Logger which gives an information to developer UC7

--- AddressBookApp/src/main/java/com/example/AddressBookApp/AddressBookAppApplication.java
+++ AddressBookApp/src/main/java/com/example/AddressBookApp/AddressBookAppApplication.java
@@ -1,13 +1,17 @@
 package com.example.AddressBookApp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AddressBookAppApplication {
+	private static final Logger log = LoggerFactory.getLogger(AddressBookAppApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(AddressBookAppApplication.class, args);
+		log.info("App has started!!!");
 	}
 
 }
